6e82747956ca30c2a6e63bff94314dffc5a7d953
irina <irina.sitaeva@yandex.ru>
2021-11-20 00:07:06 +0300

    files added

---
+++ 2-Sitaeva-C1/Source.cpp
@@ -0,0 +1,273 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(linker, "/STACK:2097152000")
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+
+/*
+* 
+* Working time 34.295452
+* 
+* 
+WindowsBuildLabEx                                       : 19041.1.amd64fre.vb_release.191206-1406
+WindowsCurrentVersion                                   : 6.3
+WindowsEditionId                                        : Professional
+WindowsInstallationType                                 : Client
+WindowsInstallDateFromRegistry                          : 09.03.2021 2:06:52
+WindowsProductId                                        : 00330-80000-00000-AA201
+WindowsProductName                                      : Windows 10 Pro
+WindowsRegisteredOrganization                           :
+WindowsRegisteredOwner                                  : Ирина
+WindowsSystemRoot                                       : C:\WINDOWS
+WindowsVersion                                          : 2009
+BiosCharacteristics                                     : {7, 11, 12, 15...}
+BiosBIOSVersion                                         : {_ASUS_ - 1072009, X512FL.303, American Megatrends - 5000D}
+BiosBuildNumber                                         :
+BiosCaption                                             : X512FL.303
+BiosCodeSet                                             :
+BiosCurrentLanguage                                     : en|US|iso8859-1
+BiosDescription                                         : X512FL.303
+BiosEmbeddedControllerMajorVersion                      : 255
+BiosEmbeddedControllerMinorVersion                      : 255
+BiosFirmwareType                                        : Uefi
+BiosIdentificationCode                                  :
+BiosInstallableLanguages                                : 1
+BiosInstallDate                                         :
+BiosLanguageEdition                                     :
+BiosListOfLanguages                                     : {en|US|iso8859-1}
+BiosManufacturer                                        : American Megatrends Inc.
+BiosName                                                : X512FL.303
+BiosOtherTargetOS                                       :
+BiosPrimaryBIOS                                         : True
+BiosReleaseDate                                         : 26.08.2019 3:00:00
+BiosSeralNumber                                         : KBN0CV09K735468
+BiosSMBIOSBIOSVersion                                   : X512FL.303
+BiosSMBIOSMajorVersion                                  : 3
+BiosSMBIOSMinorVersion                                  : 2
+BiosSMBIOSPresent                                       : True
+BiosSoftwareElementState                                : Running
+BiosStatus                                              : OK
+BiosSystemBiosMajorVersion                              : 5
+BiosSystemBiosMinorVersion                              : 13
+BiosTargetOperatingSystem                               : 0
+BiosVersion                                             : _ASUS_ - 1072009
+CsAdminPasswordStatus                                   : Unknown
+CsAutomaticManagedPagefile                              : True
+CsAutomaticResetBootOption                              : False
+CsAutomaticResetCapability                              : True
+CsBootOptionOnLimit                                     :
+CsBootOptionOnWatchDog                                  :
+CsBootROMSupported                                      : True
+CsBootStatus                                            : {0, 0, 0, 0...}
+CsBootupState                                           : Normal boot
+CsCaption                                               : DESKTOP-SSQ2LLG
+CsChassisBootupState                                    : Safe
+CsChassisSKUNumber                                      : NA
+CsCurrentTimeZone                                       : 180
+CsDaylightInEffect                                      :
+CsDescription                                           : AT/AT COMPATIBLE
+CsDNSHostName                                           : DESKTOP-SSQ2LLG
+CsDomain                                                : WORKGROUP
+CsDomainRole                                            : StandaloneWorkstation
+CsEnableDaylightSavingsTime                             : True
+CsFrontPanelResetStatus                                 : Unknown
+CsHypervisorPresent                                     : False
+CsInfraredSupported                                     : False
+CsInitialLoadInfo                                       :
+CsInstallDate                                           :
+CsKeyboardPasswordStatus                                : Unknown
+CsLastLoadInfo                                          :
+CsManufacturer                                          : ASUSTeK COMPUTER INC.
+CsModel                                                 : VivoBook_ASUSLaptop X512FL_A512FL
+CsName                                                  : DESKTOP-SSQ2LLG
+CsNetworkAdapters                                       : {Беспроводная сеть, Сетевое подключение Bluetooth}
+CsNetworkServerModeEnabled                              : True
+CsNumberOfLogicalProcessors                             : 8
+CsNumberOfProcessors                                    : 1
+CsProcessors                                            : {Intel(R) Core(TM) i5-8265U CPU @ 1.60GHz}
+CsOEMStringArray                                        : {kPJ-+7X7+EfTa, jTyRUBSNi7Ydf, fCrOzJ6x1i-eh,  ...}
+CsPartOfDomain                                          : False
+CsPauseAfterReset                                       : -1
+CsPCSystemType                                          : Mobile
+CsPCSystemTypeEx                                        : Mobile
+CsPowerManagementCapabilities                           :
+CsPowerManagementSupported                              :
+CsPowerOnPasswordStatus                                 : Unknown
+CsPowerState                                            : Unknown
+CsPowerSupplyState                                      : Safe
+CsPrimaryOwnerContact                                   :
+CsPrimaryOwnerName                                      : Ирина
+CsResetCapability                                       : Other
+CsResetCount                                            : -1
+CsResetLimit                                            : -1
+CsRoles                                                 : {LM_Workstation, LM_Server, NT, Potential_Browser...}
+CsStatus                                                : OK
+CsSupportContactDescription                             :
+CsSystemFamily                                          : VivoBook
+CsSystemSKUNumber                                       :
+CsSystemType                                            : x64-based PC
+CsThermalState                                          : Safe
+CsTotalPhysicalMemory                                   : 8434110464
+CsPhyicallyInstalledMemory                              : 8388608
+CsUserName                                              : DESKTOP-SSQ2LLG\Ирина
+CsWakeUpType                                            : PowerSwitch
+CsWorkgroup                                             : WORKGROUP
+OsName                                                  : Майкрософт Windows 10 Pro
+OsType                                                  : WINNT
+OsOperatingSystemSKU                                    : 48
+OsVersion                                               : 10.0.19042
+OsCSDVersion                                            :
+OsBuildNumber                                           : 19042
+OsHotFixes                                              : {KB5006365, KB4562830, KB4577586, KB4580325...}
+OsBootDevice                                            : \Device\HarddiskVolume4
+OsSystemDevice                                          : \Device\HarddiskVolume6
+OsSystemDirectory                                       : C:\WINDOWS\system32
+OsSystemDrive                                           : C:
+OsWindowsDirectory                                      : C:\WINDOWS
+OsCountryCode                                           : 7
+OsCurrentTimeZone                                       : 180
+OsLocaleID                                              : 0419
+OsLocale                                                : ru-RU
+OsLocalDateTime                                         : 19.11.2021 23:57:40
+OsLastBootUpTime                                        : 14.11.2021 20:11:09
+OsUptime                                                : 5.03:46:31.3007525
+OsBuildType                                             : Multiprocessor Free
+OsCodeSet                                               : 1251
+OsDataExecutionPreventionAvailable                      : True
+OsDataExecutionPrevention32BitApplications              : True
+OsDataExecutionPreventionDrivers                        : True
+OsDataExecutionPreventionSupportPolicy                  : OptIn
+OsDebug                                                 : False
+OsDistributed                                           : False
+OsEncryptionLevel                                       : 256
+OsForegroundApplicationBoost                            : Maximum
+OsTotalVisibleMemorySize                                : 8236436
+OsFreePhysicalMemory                                    : 2989568
+OsTotalVirtualMemorySize                                : 11513236
+OsFreeVirtualMemory                                     : 3871052
+OsInUseVirtualMemory                                    : 7642184
+OsTotalSwapSpaceSize                                    :
+OsSizeStoredInPagingFiles                               : 3276800
+OsFreeSpaceInPagingFiles                                : 3080404
+OsPagingFiles                                           : {C:\pagefile.sys}
+OsHardwareAbstractionLayer                              : 10.0.19041.1151
+OsInstallDate                                           : 09.03.2021 5:06:52
+OsManufacturer                                          : Microsoft Corporation
+OsMaxNumberOfProcesses                                  : 4294967295
+OsMaxProcessMemorySize                                  : 137438953344
+OsMuiLanguages                                          : {ru-RU, en-US}
+OsNumberOfLicensedUsers                                 :
+OsNumberOfProcesses                                     : 216
+OsNumberOfUsers                                         : 2
+OsOrganization                                          :
+OsArchitecture                                          : 64-разрядная
+OsLanguage                                              : ru-RU
+OsProductSuites                                         : {TerminalServicesSingleSession}
+OsOtherTypeDescription                                  :
+OsPAEEnabled                                            :
+OsPortableOperatingSystem                               : False
+OsPrimary                                               : True
+OsProductType                                           : WorkStation
+OsRegisteredUser                                        : Ирина
+OsSerialNumber                                          : 00330-80000-00000-AA201
+OsServicePackMajorVersion                               : 0
+OsServicePackMinorVersion                               : 0
+OsStatus                                                : OK
+OsSuites                                                : {TerminalServices, TerminalServicesSingleSession}
+OsServerLevel                                           :
+KeyboardLayout                                          : ru-RU
+TimeZone                                                : (UTC+03:00) Москва, Санкт-Петербург
+LogonServer                                             : \\DESKTOP-SSQ2LLG
+PowerPlatformRole                                       : Mobile
+HyperVisorPresent                                       : False
+HyperVRequirementDataExecutionPreventionAvailable       : True
+HyperVRequirementSecondLevelAddressTranslation          : True
+HyperVRequirementVirtualizationFirmwareEnabled          : True
+HyperVRequirementVMMonitorModeExtensions                : True
+DeviceGuardSmartStatus                                  : Off
+DeviceGuardRequiredSecurityProperties                   :
+DeviceGuardAvailableSecurityProperties                  :
+DeviceGuardSecurityServicesConfigured                   :
+DeviceGuardSecurityServicesRunning                      :
+DeviceGuardCodeIntegrityPolicyEnforcementStatus         :
+DeviceGuardUserModeCodeIntegrityPolicyEnforcementStatus :
+*/
+int** create_matrix(FILE* file, int size) {
+    char ch;
+    int current;
+    
+
+    int** matrix = (int**)malloc(size * sizeof(int*));
+    if (matrix == NULL) {
+        fprintf(stderr, "%s", "Error of allocting memory");
+        exit(-1);
+    }
+
+    for (int i = 0; i < size; i++) {
+        matrix[i] = (int*)calloc(size, sizeof(int));
+    }
+
+    for (int i = 0; i < size; i++) {
+        ch = ' ';
+        int a;
+        //scanf("%i", &a);
+        while (fscanf(file, "%i", &current)==1) {
+           // scanf("%i%c", &current, &ch);
+            if (current != i) {
+                matrix[i][current] = 1;
+                matrix[current][i] = 1;
+
+            }
+           if (getc(file) == '\n') break;
+        }
+    }  
+    return matrix;
+}
+
+void dfs(int** graf, int n, int size) {
+    printf("%i ", n);
+    for (int i = 0; i < size; i++) {
+        if (graf[n][i] != 0) {
+            graf[i][n] = 0;
+            graf[n][i] = 0;
+            dfs(graf, i, size);
+        }
+    }
+}
+
+void Test() {
+    srand(time(NULL));
+    int nodes_amount=2000, links_in_one_node = 50;
+    FILE* input = fopen("input.txt", "w");
+    fprintf(input, "%u\n", nodes_amount);
+
+    for (unsigned node_number = 0; node_number < nodes_amount; node_number++) {
+        fprintf(input, "%d", node_number);
+        for (unsigned links_amount = 0; links_amount <= links_in_one_node; links_amount++)
+            fprintf(input, " %d", rand() % nodes_amount);
+        fprintf(input, "\n");
+    }
+    fclose(input);
+}
+
+int main() {
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    LARGE_INTEGER start, end;
+    double std1_res, std2_res;
+    Test();
+    FILE* file = fopen("input.txt", "r");
+    QueryPerformanceCounter(&start);
+    
+    //freopen("input.txt", "r", stdin);
+    int size;
+    fscanf(file, "%i", &size);
+    int** graf;
+    graf = create_matrix(file, size);
+    dfs(graf,0, size);
+    QueryPerformanceCounter(&end);
+    std1_res = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
+    printf("Working time %f\n",std1_res); 
+}
